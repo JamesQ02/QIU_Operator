d1c01a77d72af3685669e5758cc0ff658f94d366
JamesQ02 <77318371+JamesQ02@users.noreply.github.com>
2021-02-26 15:49:45 +0800

    Add files via upload

---
+++ app/src/main/java/mcm/edu/ph/qiu_operator/MainActivity.java
@@ -0,0 +1,55 @@
+package mcm.edu.ph.qiu_operator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView display;
+        display = findViewById(R.id.txtview);
+
+
+        int birthYear = 2002;
+        int birthMonth = 7;
+        int birthDay = 9;
+
+        int currentYear= 2021;
+        int currentMonth = 2;
+        int currentDay = 26;
+
+        int ageInYears,ageInMonths,ageInDays;
+
+        if(birthMonth > currentMonth){
+            currentMonth += 12;
+            currentYear -= 1;
+        }
+
+        if(birthDay > currentDay){
+            currentDay += 30;
+            currentMonth -= 1;
+        }
+
+        ageInYears = currentYear - birthYear;
+        ageInMonths = currentMonth - birthMonth;
+        ageInDays = currentDay - birthDay;
+
+        display.setText("My precise age is \n"+
+                String.valueOf(ageInYears)+
+                " Years,\n"+
+                String.valueOf(ageInMonths)+
+                " Months,\n"+
+                String.valueOf(ageInDays)+
+                " Days.");
+
+
+
+
+    }
+}
